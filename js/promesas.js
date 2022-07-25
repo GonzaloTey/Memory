3bd23972d8a15818306915a437fbc00d20860710
Gonzalo Tey <gontey@hotmail.com>
2022-07-25 11:49:42 -0300

    Agregando metodo moderno de promesas. Listo para entrega Final

--- js/promesas.js
+++ js/promesas.js
@@ -2,7 +2,12 @@ const juegos = document.getElementById("juegosNuevos");
 const verJuegos = document.getElementById("verJuegos");
 const demoraContainer = document.querySelector(".demoraContainer")
 
-const llamarJuegos = async () => {
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//+++++++Codigo NO borrado ya que quiero tener los dos para practicarlos. Metodo Viejo y Nuevo+++++++++++
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+/* const llamarJuegos = async () => {
     const response = await fetch('/datos.json');
     const data = await response.json();
 
@@ -26,15 +31,48 @@ const llamarJuegos = async () => {
         marco.classList.add("col-12", "col-sm-6", "col-xl-3", "shadow", "p-3", "mb-5", "bg-body", "rounded")
         juegos.append(marco)
     });
-}
+} */
+
+const llamarJuegos = () => {
+    fetch('/datos.json')
+        .then((response) => response.json())
+        .then((data) => {
+            data.forEach(element => {
+                const marco = document.createElement("div")
+
+                marco.innerHTML = `
+                            <div class="card h-100 bg-light">
+                                <img src= "../assets/juego${element.id}.jpg"
+                                class="card-img-top" alt="juegos">
+                                <div class="card-body fs-5">
+                                    <h5 class="card-title">${element.nombre}</h5>
+                                    <p class="card-text">
+                                    - Dificultad: ${element.dificultad}.<br>
+                                    - Estado: ${element.estado}.<br>
+                                    - Edad: ${element.edad}</p>
+
+                                </div>
+                                <button type="button" class="btn btn-secondary disabled">Jugar!</button>
+                            </div>`
+                marco.classList.add("col-12", "col-sm-6", "col-xl-3", "shadow", "p-3", "mb-5", "bg-body", "rounded")
+                juegos.append(marco)
+            });
+        })
+        .catch(() => {
+            demoraContainer.innerHTML = `<h3>Problemas con el servidor. Probar en otro momento</h3>`
+        })
+        .finally(() => {
+            demoraContainer.innerHTML = `<h3>Disponibles a partir del proximo mes</h3>`
+        })
+};
 
 const timer = () => {
     const demora = document.createElement("h3");
     demora.textContent = "La carga puede demorar unos segundos...";
     demoraContainer.append(demora);
     setTimeout(() => {
-        llamarJuegos();
-        demora.textContent = " "
+        demora.textContent = " ";
+        llamarJuegos()
     }, 2500);
 }
 
